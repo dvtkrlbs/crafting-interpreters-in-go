12c3cb27f310bfdcfc43eecb51e46bc722f04b88
Tunahan Karlibas <dvt.tnhn.krlbs@icloud.com>
2024-05-06 07:14:39 +0000

    base lexer

--- token/token.go
+++ token/token.go
@@ -5,6 +5,8 @@ type TokenType string
 type Token struct {
 	Type    TokenType
 	Literal string
+}
+
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF = "EOF"
@@ -19,7 +21,7 @@ const (
 
 	/// Delimiters
 	COMMA = ","
-	SEMICCOLON = ";"
+	SEMICOLON = ";"
 
 	LPAREN = "("
 	RPAREN = ")"
@@ -30,3 +32,16 @@ const (
 	FUNCTION = "FUNCTION"
 	LET = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
